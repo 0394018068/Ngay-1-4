06c83bf0de9b464572cb573ab9c5dd1c71664353
0394018068 <longvipbox@gmail.com>
2026-04-01 09:04:35 +0700

    Update

--- models/Product.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number
-}, { timestamps: true });
-
-module.exports = mongoose.model('Product', productSchema);
--- routes/productRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/productController');
-
-router.post('/', controller.createProduct);
-
-module.exports = router;
